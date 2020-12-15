944c1fc18bc70a0bf257d7c20dcf304ceaddcf6b
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-15 19:43:33 +0700

    Finale

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float speed = 3;
+    GameObject player;
 
+    public GameObject panelWin;
+
+    public AudioSource win;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        MoveVertical();
+    }
+
+    void MoveVertical()
+    {
+        float z = Input.GetAxis("Vertical");
+        Vector3 dirZ = new Vector3(0, 0, z);
+        Debug.Log("Berhasil Vertical");
+        player.transform.Translate(dirZ.normalized * Time.deltaTime * speed);
+
+    }
+
+    private void OnCollisionEnter(Collision Menang)
+    {
+        if (SceneManager.GetActiveScene().name == "Level 1")
+        {
+            if (Menang.collider.CompareTag("Win"))
+            {
+                win.Play();
+                Debug.Log("Winn");
+                panelWin.SetActive(true);
+                Time.timeScale = 0;
+            }
+        } else if (SceneManager.GetActiveScene().name == "Level 2")
+        {
+            if (Menang.collider.CompareTag("Win"))
+            {
+                win.Play();
+                Debug.Log("Winn");
+                panelWin.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
     }
 }
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Deklarasi
+    public GameObject infoPanel;
+
+    #endregion
+
+    #region Menu
+    public void Info()
+    {
+        infoPanel.SetActive(true);
+
+    }
+
+    public void BackInfo()
+    {
+        infoPanel.SetActive(false);
+
+    }
+
+    public void Intro()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Intro
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    #endregion
+
+    #region level1
+
+    public void Level1()
+    {
+        if (SceneManager.GetActiveScene().name == "Level 1")
+        {
+            if (Input.GetKey(KeyCode.Y))
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+            else if (Input.GetKey(KeyCode.N))
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+
+    }
+    #endregion
+
+    #region level2
+
+    public void Level2()
+    {
+        if (SceneManager.GetActiveScene().name == "Level 2")
+        {
+            if (Input.GetKey(KeyCode.Y))
+            {
+                SceneManager.LoadScene("Level 1");
+            } else if (Input.GetKey(KeyCode.N))
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+        
+    }
+    #endregion
+
+    private void Update()
+    {
+        Level1();
+        Level2();
+    }
+
+
+
+
+
+
+
+
+
+
+}
